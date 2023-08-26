10dc007cf698ddb74228d632262355466d8987a3
lightsing <light.tsing@gmail.com>
2023-08-26 22:24:56 +0800

    migrate

--- migrations/20230826064813_init.sql
+++ migrations/20230826064813_init.sql
@@ -1,26 +1,26 @@
-CREATE TABLE IF NOT EXISTS block_status
+CREATE TABLE IF NOT EXISTS block
 (
     block_number INTEGER PRIMARY KEY,
     tx_fetched   INTEGER DEFAULT 0
 );
 
-CREATE TABLE IF NOT EXISTS tx_status
+CREATE TABLE IF NOT EXISTS tx
 (
     block_number INTEGER,
     tx_index     INTEGER,
     tx_hash      BLOB PRIMARY KEY,
     tx_analyzed  INTEGER DEFAULT 0,
-    FOREIGN KEY (block_number) REFERENCES block_status (block_number)
+    FOREIGN KEY (block_number) REFERENCES block (block_number)
 );
 
-CREATE INDEX IF NOT EXISTS idx_tx_status_block_number ON tx_status (block_number);
+CREATE INDEX IF NOT EXISTS idx_tx_status_block_number ON tx (block_number);
 
 CREATE TABLE IF NOT EXISTS opcode_statistics
 (
     block_number INTEGER,
     opcode       INTEGER,
     count        INTEGER,
-    FOREIGN KEY (block_number) REFERENCES block_status (block_number),
+    FOREIGN KEY (block_number) REFERENCES block (block_number),
     UNIQUE (block_number, opcode)
 );
 
--- src/db.rs
+++ src/db.rs
@@ -27,7 +27,7 @@ pub async fn init_sqlite() -> Result<SqlitePool, sqlx::Error> {
 
 pub async fn get_latest_recorded_block(pool: &SqlitePool) -> Result<u64, sqlx::Error> {
     Ok(
-        sqlx::query!("SELECT block_number FROM block_status ORDER BY block_number DESC LIMIT 1")
+        sqlx::query!("SELECT block_number FROM block ORDER BY block_number DESC LIMIT 1")
             .fetch_optional(pool)
             .await?
             .map(|row| row.block_number as u64)
@@ -38,7 +38,7 @@ pub async fn get_latest_recorded_block(pool: &SqlitePool) -> Result<u64, sqlx::E
 pub async fn append_block_task(pool: &SqlitePool, block_number: u64) -> Result<(), sqlx::Error> {
     let block_number = block_number as i64;
     sqlx::query!(
-        "INSERT INTO block_status (block_number) VALUES (?) ON CONFLICT(block_number) DO NOTHING",
+        "INSERT INTO block (block_number) VALUES (?) ON CONFLICT(block_number) DO NOTHING",
         block_number,
     )
     .execute(pool)
@@ -56,7 +56,7 @@ pub async fn append_tx_task(
     let tx_index = tx_index as i64;
     let hash = tx_hash.as_ref();
     sqlx::query!(
-        "INSERT INTO tx_status (block_number, tx_index, tx_hash) VALUES (?, ?, ?) ON CONFLICT(tx_hash) DO NOTHING",
+        "INSERT INTO tx (block_number, tx_index, tx_hash) VALUES (?, ?, ?) ON CONFLICT(tx_hash) DO NOTHING",
         block_number,
         tx_index,
         hash,
@@ -67,17 +67,16 @@ pub async fn append_tx_task(
 }
 
 pub async fn acquire_block_task(pool: &SqlitePool) -> Result<Option<u64>, sqlx::Error> {
-    let block_number =
-        sqlx::query!("SELECT block_number FROM block_status WHERE tx_fetched = 0 LIMIT 1",)
-            .fetch_optional(pool)
-            .await?
-            .map(|row| row.block_number);
+    let block_number = sqlx::query!("SELECT block_number FROM block WHERE tx_fetched = 0 LIMIT 1",)
+        .fetch_optional(pool)
+        .await?
+        .map(|row| row.block_number);
     if block_number.is_none() {
         return Ok(None);
     }
     let block_number = block_number.unwrap();
     sqlx::query!(
-        "UPDATE block_status SET tx_fetched = 2 WHERE block_number = ?",
+        "UPDATE block SET tx_fetched = 2 WHERE block_number = ?",
         block_number,
     )
     .execute(pool)
@@ -91,7 +90,7 @@ pub async fn mark_block_task_fetched(
 ) -> Result<(), sqlx::Error> {
     let block_number = block_number as i64;
     sqlx::query!(
-        "UPDATE block_status SET tx_fetched = 1 WHERE block_number = ? AND tx_fetched = 2",
+        "UPDATE block SET tx_fetched = 1 WHERE block_number = ? AND tx_fetched = 2",
         block_number,
     )
     .execute(pool)
@@ -100,7 +99,7 @@ pub async fn mark_block_task_fetched(
 }
 
 pub async fn acquire_tx_task(pool: &SqlitePool) -> Result<Option<H256>, sqlx::Error> {
-    let tx_hash = sqlx::query!("SELECT tx_hash FROM tx_status WHERE tx_analyzed = 0 LIMIT 1",)
+    let tx_hash = sqlx::query!("SELECT tx_hash FROM tx WHERE tx_analyzed = 0 LIMIT 1",)
         .fetch_optional(pool)
         .await?
         .map(|row| row.tx_hash);
@@ -108,12 +107,9 @@ pub async fn acquire_tx_task(pool: &SqlitePool) -> Result<Option<H256>, sqlx::Er
         return Ok(None);
     }
     let tx_hash = tx_hash.unwrap().unwrap();
-    sqlx::query!(
-        "UPDATE tx_status SET tx_analyzed = 2 WHERE tx_hash = ?",
-        tx_hash,
-    )
-    .execute(pool)
-    .await?;
+    sqlx::query!("UPDATE tx SET tx_analyzed = 2 WHERE tx_hash = ?", tx_hash,)
+        .execute(pool)
+        .await?;
     Ok(Some(H256::from_slice(&tx_hash)))
 }
 
@@ -121,7 +117,7 @@ pub async fn acquire_tx_task(pool: &SqlitePool) -> Result<Option<H256>, sqlx::Er
 pub async fn mark_tx_task_analyzed(pool: &SqlitePool, tx_hash: H256) -> Result<(), sqlx::Error> {
     let hash = tx_hash.as_ref();
     let result = sqlx::query!(
-        "UPDATE tx_status SET tx_analyzed = 1 WHERE tx_hash = ? AND tx_analyzed = 2",
+        "UPDATE tx SET tx_analyzed = 1 WHERE tx_hash = ? AND tx_analyzed = 2",
         hash,
     )
     .execute(pool)
@@ -152,10 +148,10 @@ pub async fn append_opcode_statistics(
 }
 
 pub async fn clear_pending_tasks(pool: &SqlitePool) -> Result<(), sqlx::Error> {
-    sqlx::query!("UPDATE block_status SET tx_fetched = 0 WHERE tx_fetched = 2",)
+    sqlx::query!("UPDATE block SET tx_fetched = 0 WHERE tx_fetched = 2",)
         .execute(pool)
         .await?;
-    sqlx::query!("UPDATE tx_status SET tx_analyzed = 0 WHERE tx_analyzed = 2",)
+    sqlx::query!("UPDATE tx SET tx_analyzed = 0 WHERE tx_analyzed = 2",)
         .execute(pool)
         .await?;
     Ok(())
